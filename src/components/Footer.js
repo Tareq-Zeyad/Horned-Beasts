ec9b6d379f2fc53c89e8e00642247109020e7f36
Tariq Zeyad Sharaia <tariq.z.sharaia@gmail.com>
2021-08-30 14:49:36 +0300

    lab01 completed

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component{
+    render(){
+        return(
+        <>
+        <span>Tariq Sharaia &copy; 2021</span>
+        </>
+        )
+    }
+}
+
+export default Footer;
