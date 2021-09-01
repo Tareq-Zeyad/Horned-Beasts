bd8d2cb81a7fcc40bfc8078d7ddb92287c62b967
Tariq Zeyad Sharaia <tariq.z.sharaia@gmail.com>
2021-09-01 15:06:30 +0300

    lab03 completed

--- src/components/SelectedBeast.js
+++ src/components/SelectedBeast.js
@@ -6,22 +6,26 @@ import Card from 'react-bootstrap/Card';
 
 class SelectedBeast extends React.Component {
 
+    handleClose = () =>{
+        this.props.handleClose();
+    }
+
     render() {
         return (
             <>
 
-                <Modal show={this.props.show} onHide={this.props.handleClose}>
+                <Modal show={this.props.show} onHide={this.handleClose}>
                     <Modal.Header closeButton>
                         <Modal.Title>{this.props.title}</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
                         <Card>
-                        <img variant="top" src={this.props.img} onClick={this.incrementNumberOfVotes}/>
+                        <img variant="top" src={this.props.src} onClick={this.incrementNumberOfVotes}/>
                         <p> {this.props.description};</p>
                         </Card>
                     </Modal.Body>
                     <Modal.Footer>
-                        <Button variant="secondary" onClick={this.props.handleClose}>
+                        <Button variant="primary" onClick={this.props.handleClose}>
                             Close
                         </Button>
                     </Modal.Footer>
