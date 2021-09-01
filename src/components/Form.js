918961b80ac7b05b375052b41897902879ffc5d4
Tariq Zeyad Sharaia <tariq.z.sharaia@gmail.com>
2021-09-01 22:20:30 +0300

    not completed

---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from 'react-bootstrap';
+
+class Form extends React.component{
+    render () {
+        return(
+            <>
+
+            </>
+        )
+    }
+}
+
+export default Form;
