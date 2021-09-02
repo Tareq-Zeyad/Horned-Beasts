0ef1f9373861b543e8108e314c9f5013b1088469
Tariq Zeyad Sharaia <tariq.z.sharaia@gmail.com>
2021-09-02 13:52:39 +0300

    lab04 completed

--- src/App.js
+++ src/App.js
@@ -4,44 +4,72 @@ import Main from './components/Main';
 import Footer from './components/Footer';
 import rawData from './components/HornedBeasts.json'
 import SelectedBeast from './components/SelectedBeast';
+import Form from 'react-bootstrap/Form'
 
 class App extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state={
-            Data : rawData,
-            show : false,
+        this.state = {
+            Data: rawData,
+            show: false,
             // because these values from the object array are dynamic data that will move to display from a card to a modal
-            title : '',
-            src : '',
-            description : '',
+            title: '',
+            src: '',
+            description: '',
         };
-        
+
     }
 
     handleShow = () => {
         this.setState({
-          show : true
+            show: true
         });
     }
 
     handleClose = () => {
-        this .setState({
-            show : false
+        this.setState({
+            show: false
         })
     }
 
-    updateSelectedBeastsData = (title,src,description) => {
+    updateSelectedBeastsData = (title, src, description) => {
         this.setState({
             title: title,
-            src : src,
-            description : description,
+            src: src,
+            description: description,
         })
 
     }
 
-    
+    selectHorn = (event) => {
+        console.log(event.target.value);
+        let beastsFiltering
+
+        if (event.target.value == 'All') {
+            beastsFiltering = rawData;
+        }
+        else {
+            beastsFiltering = rawData.filter(item => {
+                if (item.horns === parseInt(event.target.value)) {
+                    return item
+                }
+
+
+
+            })
+            console.log(beastsFiltering);
+
+            
+        }
+        this.setState({
+            Data: beastsFiltering,
+        });
+
+
+    };
+
+
 
     render() {
         // console.log(Data);
@@ -49,17 +77,31 @@ class App extends React.Component {
         return (
             <>
                 <Header />
-                <Main 
-               Data= {this.state.Data}
-               handleShow={this.handleShow}
-               updateSelectedBeastsData={this.updateSelectedBeastsData}
+
+                <Form>
+                    <Form.Group controlId="horns">
+                        <Form.Select aria-label="Select a horn number:" onChange={this.selectHorn}>
+                            <option>Select beast horns number:</option>
+                            <option value="All">All</option>
+                            <option value="1">One horn</option>
+                            <option value="2">Two horns</option>
+                            <option value="3">Three horns</option>
+                            <option value="100">Wow</option>
+                        </Form.Select>
+                    </Form.Group>
+                </Form>
+                <Main
+
+                    Data={this.state.Data}
+                    handleShow={this.handleShow}
+                    updateSelectedBeastsData={this.updateSelectedBeastsData}
                 />
                 <SelectedBeast
-                show={this.state.show}
-                handleClose={this.handleClose}
-                title={this.state.title}
-                src={this.state.src}
-                description={this.state.description}
+                    show={this.state.show}
+                    handleClose={this.handleClose}
+                    title={this.state.title}
+                    src={this.state.src}
+                    description={this.state.description}
                 />
                 <Footer />
             </>
